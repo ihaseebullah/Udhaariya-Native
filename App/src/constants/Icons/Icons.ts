3b7e44b8832da1aaa692d879fe4784f464369031
Haseeb Ullah <ihaseebullahtarakai@gmail.com>
2025-02-24 20:22:49 +0500

    Updated Bottom Tab Bar

--- App/src/constants/Icons/Icons.ts
+++ App/src/constants/Icons/Icons.ts
@@ -2,8 +2,14 @@
 import InfoIcon from '../../assets/icons/info-circle.svg';
 import NextIcon from '../../assets/icons/chevron-right.svg';
 import BackIcon from '../../assets/icons/arrow-left.svg';
+import WalletIcon from '../../assets/icons/wallet-03.svg';
+import UsersIcon from '../../assets/icons/users-02.svg';
+import HomeIcon from '../../assets/icons/home-03.svg';
 export const Icons = {
   info: InfoIcon,
   Next: NextIcon,
   back: BackIcon,
+  Home: HomeIcon,
+  Wallet: WalletIcon,
+  Users: UsersIcon,
 };
